6d75ec5aec559cdd4dbded3ce5f0f5fb2bf861fa
Molnar Tamas <m.memot@gmail.com>
2026-09-01 20:55:50 +0200

    Javitas: Jelszo ellenorzes a jelszo hash alapjan

--- p_login.php
+++ p_login.php
@@ -7,13 +7,13 @@
     $jl = $_POST["jl"];
     
     //SELECT u.dolgozo_id, u.jogkor, d.dolgozo_nev, d.beosztas FROM users u JOIN dolgozok d ON u.dolgozo_id = d.dolgozo_id WHERE u.usernev = 'memot' AND u.jelszo = 'jelszo11' AND u.torolve IS NULL
-    $sql = "SELECT u.dolgozo_id, u.jogkor, d.dolgozo_nev, d.beosztas 
+    $sql = "SELECT u.dolgozo_id, u.jogkor, u.jelszo_hash, d.dolgozo_nev, d.beosztas 
             FROM users u 
             JOIN dolgozok d ON u.dolgozo_id = d.dolgozo_id 
-            WHERE u.usernev = ? AND u.jelszo = ? AND (u.torolve IS NULL or u.torolve = 0)";
+            WHERE u.usernev = ? AND (u.torolve IS NULL or u.torolve = 0)";
     $stmt = $conn->prepare($sql);
 
-    $stmt->bind_param("ss", $fh, $jl);
+    $stmt->bind_param("s", $fh);
 
     $stmt->execute();
     $result = $stmt->get_result();
@@ -22,6 +22,16 @@
     if ($result && $result->num_rows > 0) // helyes és ad vissza eredménysort, azaz van ilyen felhasználó
         {
             $row = $result->fetch_assoc();
+
+            // Ellenőrizzük a jelszó hash-t a megadott jelszóval
+            if (!password_verify($jl, $row["jelszo_hash"])) {
+                // Hibás jelszó esetén átirányítás a sikertelen.php oldalra
+                $stmt->close();
+                $conn->close();
+                header("Location:sikertelen.php");
+                exit();
+            }
+
             session_start();
             session_unset();
             session_destroy();
